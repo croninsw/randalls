d42e6b09d06c253fef66890c61d0f1baeded7b4d
Sam Cronin <croninsw@gmail.com>
2019-02-05 14:17:43 -0600

    Finished daily challenges and exercises

--- carsales.js
+++ carsales.js
@@ -1,16 +1,16 @@
 // DISPLAY AGENTS NAME ON DOM WITH CAR ETC.
-const outputElement = document.querySelector("#weeklySales")
 // const weeklySales = salesByWeek.forEach(sale => {
-//     outputElement.innerHTML += `<div><h3>${sale.sales_agent.first_name} ${sale.sales_agent.last_name}</h3></div>`
-
-//     for (entry of Object.entries(sale.vehicle)) {
-//         outputElement.innerHTML += `<div>${entry[0]}: ${entry[1]}</div>`
-//     }
-
-//     outputElement.innerHTML += `<div><h3>Profit: $${sale.gross_profit}</h3></div>`
-//     outputElement.innerHTML += "<hr />"
-// })
-
+  //     outputElement.innerHTML += `<div><h3>${sale.sales_agent.first_name} ${sale.sales_agent.last_name}</h3></div>`
+  
+  //     for (entry of Object.entries(sale.vehicle)) {
+    //         outputElement.innerHTML += `<div>${entry[0]}: ${entry[1]}</div>`
+    //     }
+    
+    //     outputElement.innerHTML += `<div><h3>Profit: $${sale.gross_profit}</h3></div>`
+    //     outputElement.innerHTML += "<hr />"
+    // })
+    
+const outputElement = document.querySelector("#weeklySales")
 const searchInput = document.querySelector("#searchInput")
 
 searchInput.addEventListener("keypress", (event) => {
@@ -18,9 +18,16 @@ searchInput.addEventListener("keypress", (event) => {
     const searchTerm = event.target.value
 
     salesByWeek.forEach(sale => {
-        if (searchTerm === Object.values(sale.sales_agent)) {
-        outputElement.innerHTML += `<div> ${sale} </div>`
+      for (const value of Object.values(sale.sales_agent)) {
+        if (value.includes(searchTerm)) {
+          outputElement.innerHTML += `
+              <div>${sale.sales_agent.first_name} ${sale.sales_agent.last_name}</div>
+              <div>Agent Email: ${sale.sales_agent.email}<div>
+              <div>Total Sales: $${sale.gross_profit}</div>
+          `
+          outputElement.innerHTML += "<hr>"
         }
-    });
+      }
+    })
   }
 })
